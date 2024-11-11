f2624bf2eaff5061a4c2ac942e6b2f26eb6e51a2
tanjimmm <tanjimpromise252000@gmail.com>
2024-11-11 13:42:09 +0600

    Added php validation for Name and ID

--- process.php
+++ process.php
@@ -8,16 +8,42 @@
 <body>
     <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
     
-    $studentName = htmlspecialchars($_POST['studentName'] ?? '');
-    $studentID = htmlspecialchars($_POST['studentID'] ?? '');
-    $bookSelected = htmlspecialchars($_POST['books'] ?? '');
-    $borrowDate = htmlspecialchars($_POST['borrow-date'] ?? '');
-    $token = htmlspecialchars($_POST['token'] ?? '');
-    $returnDate = htmlspecialchars($_POST['return-date'] ?? '');
-    $fees = htmlspecialchars($_POST['fees'] ?? '');
-    $paidStatus = htmlspecialchars($_POST['paid'] ?? '');
+    $studentName = ($_POST['studentName'] ?? '');
+    $studentID = ($_POST['studentID'] ?? '');
+    $bookSelected = ($_POST['books'] ?? '');
+    $borrowDate = ($_POST['borrowdate'] ?? '');
+    $token = ($_POST['token'] ?? '');
+    $returnDate = ($_POST['returndate'] ?? '');
+    $fees = ($_POST['fees'] ?? '');
+    $paidStatus = ($_POST['paid'] ?? '');
+
+    if (!preg_match("/[A-za-z\-]/", $_POST['studentName']))
+    {
+        echo "Name is Invalid";
+        return;
+    }
+
+    if (!preg_match("/\d{2}-\d{5}-\d{1}/", $_POST['studentID']))
+    {
+        echo "Student ID is Invalid";
+        return;
+    }
+
+    $date1 = new DateTime($borrowDate);
+    $date2 = new DateTime($returnDate);
+
+    $interval = date_diff($date1, $date2);
+
+    if ( ($interval->format('%d')) >10 )
+    {
+        echo "Can't Borrow a Book for more than 10 days";
+        return;
+    }
+
+
 
     echo "<h2>Form Data Submitted</h2>";
     echo "<p><strong>Student Name:</strong> $studentName</p>";
@@ -28,7 +54,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<p><strong>Return Date:</strong> $returnDate</p>";
     echo "<p><strong>Fees:</strong> $fees</p>";
     echo "<p><strong>Paid Status:</strong> $paidStatus</p>";
-} else {
+} 
+else 
+{
     echo "No data submitted.";
 }
 
